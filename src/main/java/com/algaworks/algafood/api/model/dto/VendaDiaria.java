3779fec0c6237ae0284ffb85de895555ceb7973f
william.gomes <william.gomes@ITBL002140D.itbam.org.br>
2021-07-23 15:25:12 -0400

    Atualização 23-07-2021

---
+++ src/main/java/com/algaworks/algafood/api/model/dto/VendaDiaria.java
@@ -0,0 +1,19 @@
+package com.algaworks.algafood.api.model.dto;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class VendaDiaria {
+	
+	private Date data;
+	private Long totalVendas;
+	private BigDecimal totalFaturado;
+
+}
