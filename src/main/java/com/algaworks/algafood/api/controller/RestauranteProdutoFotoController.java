d0d3c7df1784192bdf62f83c125ac1b9bab5871a
william.gomes <william.gomes@ITBL002140D.itbam.org.br>
2021-07-26 14:31:40 -0400

    Terminando o resto do commit

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -1,10 +1,16 @@
 package com.algaworks.algafood.api.controller;
 
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
 public class RestauranteProdutoFotoController {
+	
+	@PutMapping
+	public void atualizarFoto() {
+		
+	}
 
 }
